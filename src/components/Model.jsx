47d078878d3c6406b0e8ae070b916915bea6e692
NXGenn <165067489+NXGenn@users.noreply.github.com>
2024-09-02 14:58:01 +0530

    Initial commit

---
+++ src/components/Model.jsx
@@ -0,0 +1,30 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useState } from 'react'
+import ModelView from './ModelView'
+
+
+const Model = () => {
+  const [size, setSize] =  useState('small');
+  const [Model, setModel] = useState({
+    tittle: 'iphone 15 pro in Natural Titatinum ' 
+  })
+  useGSAP(()=>{
+    gsap.to('#heading', {y:0, opacity:1})
+  },[])
+  return (
+    <section className='common-padding'>
+      <div className='screen-max-width'>
+        <h1 id='heading' className='section-heading' >Take a closer look.</h1>
+
+        <div className='flex flex-col items-center mt-5'>
+          <div className='w-full h-[75vh]  md:h-[90vh] overflow-hidden'>
+            <ModelView/>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Model
